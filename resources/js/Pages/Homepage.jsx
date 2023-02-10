096f79396e814e619e16c5af112b39f80e4d37a5
Fathur Walkers <fathurwalkers44@gmail.com>
2023-02-10 01:08:28 -0800

    Belajar Laravel x ReactJS

---
+++ resources/js/Pages/Homepage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Homepage = () => {
+    return (
+        <>
+            <div className="card shadow-md m-4 text-black-50">
+                <p className="text-black">Hello World!</p>
+            </div>
+        </>
+    )
+}
+
+export default Homepage
